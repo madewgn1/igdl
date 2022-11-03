6944d2528e878ba01d58959dd089ad66683980f7
madewgn <mad.ewgn2@gmail.com>
2022-11-03 12:06:55 +0000

    fix all

--- bot.py
+++ bot.py
@@ -38,7 +38,7 @@ xbot = Client('InstaDL',
 # Start
 @xbot.on_message(filters.command('start') & filters.private)
 async def start(bot, update):
-  await update.reply("I'm InstagramDL!\nYou can download instagram url video/image using this bot")
+  await update.reply("I'm InstagramDL!\nYou can download instagram url video/image using this bot",True, reply_markup=InlineKeyboardMarkup(START_BUTTONS))
 
 # Downloader for tiktok
 @xbot.on_message(filters.regex(pattern='.*http.*') & filters.private)
@@ -56,14 +56,16 @@ async def insta(bot, update):
         result = dl(url)
         cak = acak()
         r = requests.get(result, allow_redirects=True)
-        if ".mp4" in r:
+        if ".mp4" in result:
             open(f'{cak}.mp4', 'wb').write(r.content)
+            print("dl berhasil otw upload")
             await bot.send_video(update.chat.id, f'{cak}.mp4',)
-            shutil.rmtree(dirs)
+            os.remove(f"{cak}.mp4")
         else:
             print("gambar")
-
- 
+            open(f'{cak}.jpg', 'wb').write(r.content)
+            await bot.send_photo(update.chat.id, f'{cak}.jpg',)
+            os.remove(f"{cak}.jpg")
 #  await update.reply('Select the options below', True, reply_markup=InlineKeyboardMarkup(DL_BUTTONS))
 
 print("bot aktif!")
